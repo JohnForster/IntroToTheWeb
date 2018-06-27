97c0a66fc3c2d493cc84f482b73cb70c060f8772
John Forster <j_forster@hotmail.co.uk>
2018-06-27 14:25:07 +0100

    Basic infrastructure set up.
    Gemfile, rspec and config.ru included.

--- myapp.rb
+++ myapp.rb
@@ -13,7 +13,8 @@ get '/route2' do
   'The time now is: ' + Time.now.to_s
 end
 
-get '/named-cat' do
+post '/named-cat' do
+  p params
   @name = params[:name]
   erb(:index)
 end
@@ -21,4 +22,8 @@ end
 get '/random-cat' do
   @name = %w[Amigo Oscar Viking].sample
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
